f7f822a15d60dd7b6067fd32dcb30c37e0990536
Sedov <namesjoe@gmail.com>
2023-10-22 15:37:16 +0300

    правки по конфигу

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     SERVER_PORT: int = 8000
     CLIENT_RECEIVE_MESSAGES: bool = True
     CLIENT_SEND_MESSAGE: str = "Hello world!"
-    CLIENT_SEND_RECIPIENT: str = None
+    CLIENT_SEND_RECIPIENT: str = "Tom Jones"
     CLIENT_SEND_COMMENT: str = "This is a test comment."
     CLIENT_SEND_FILE_PATH: str = "example.txt"
     MAX_MESSAGES: int = 20
