920f7f58068a9a2a064b587faeb905bb600b8082
Sedov <namesjoe@gmail.com>
2023-10-23 14:52:14 +0300

    првки по замечаниям

--- client.py
+++ client.py
@@ -1,62 +1,48 @@
 import asyncio
-import base64
-import json
-
 import websockets
-
+import json
+import argparse
 from config import Settings
-from logger import setup_logger, get_logger
 
 settings = Settings()
-setup_logger('client.log')
-logger = get_logger('server')
-
 
 async def receive_messages():
     async with websockets.connect(f"ws://{settings.SERVER_HOST}:{settings.SERVER_PORT}/connect") as ws:
         try:
             async for message in ws:
                 data = json.loads(message)
-                print("Received message:", data)
+                sender = data.get("sender", "Unknown")
+                message_text = data.get("message", "")
+                print(f"{sender}: {message_text}")
         except websockets.exceptions.ConnectionClosed as exc:
             print(f"Connection closed unexpectedly: {exc}")
 
-
-async def send_message(message, recipient=None, comment=None, file_path=None):
-    data = {"message": message}
-    if recipient:
-        data["recipient"] = recipient
-    if comment:
-        data["comment"] = comment
-    if file_path:
-        with open(file_path, "rb") as file:
-            file_content = file.read()
-            file_content_base64 = base64.b64encode(file_content).decode("utf-8")
-            data["file_upload"] = True
-            data["file_content"] = file_content_base64
-
+async def send_message(sender, message):
     async with websockets.connect(f"ws://{settings.SERVER_HOST}:{settings.SERVER_PORT}/connect") as ws:
         try:
+            data = {
+                "sender": sender,
+                "message": message
+            }
             await ws.send(json.dumps(data))
         except websockets.exceptions.ConnectionClosed as exc:
             print(f"Connection closed unexpectedly: {exc}")
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Chat Client")
+    parser.add_argument("sender_name", help="Name of the sender")
+    return parser.parse_args()
 
 async def main():
-    tasks = []
-    if settings.CLIENT_RECEIVE_MESSAGES:
-        tasks.append(receive_messages())
+    args = parse_arguments()
+    sender_name = args.sender_name
 
-    send_message_args = (
-        settings.CLIENT_SEND_MESSAGE,
-        settings.CLIENT_SEND_RECIPIENT,
-        settings.CLIENT_SEND_COMMENT,
-        settings.CLIENT_SEND_FILE_PATH
-    )
-    tasks.append(send_message(*send_message_args))
+    # Start the receive_messages function in the background
+    receive_messages_task = asyncio.create_task(receive_messages())
 
-    await asyncio.gather(*tasks)
-
+    while True:
+        message = input("Enter your message: ")
+        await send_message(sender_name, message)
 
 if __name__ == '__main__':
     asyncio.run(main())
